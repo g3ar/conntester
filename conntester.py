cfd508319091d8c594cd67d4b767ec62a86cabe6
gear <misty.g3ar@gmail.com>
2021-04-07 22:25:58 +0300

    Interface in progress

--- conntester.py
+++ conntester.py
@@ -1,31 +1,85 @@
 import configparser
+import queue
+from statistics import mean
+from datetime import datetime
+from time import sleep
 
 from ping3 import ping as p3p
-
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 class ConnTester():
     config = configparser.ConfigParser()
-    hosts = []
-    ping_timeout = 1
+    host = "8.8.8.8"
+    timeout = 1
+    interval = 1
+    results = queue.Queue(60)
+    app = QApplication([])
+    icon = QIcon("icon.png")
+    tray = QSystemTrayIcon()
 
     def __init__(self):
         self.config.read('conntester.ini')
-        self.hosts = self.config.get('MAIN', 'hosts').split(', ')
-        self.ping_timeout = int(self.config.get('MAIN', 'timeout'))
+        self.host = self.config.get('MAIN', 'host')
+        self.timeout = int(self.config.get('MAIN', 'timeout'))
+        self.interval = int(self.config.get('MAIN', 'interval'))
     
     def ping(self, host):
         """
         Pings specified host and returns ping time in ms
         """
-        return p3p(host, timeout=self.ping_timeout, unit='ms')
+        return p3p(host, timeout=self.timeout, unit='ms')
     
-    def ping_all(self):
+    def add_result(self, r):
         """
-        Pings all hosts from config
+        Add result to queue
         """
-        for host in self.hosts:
-            print(f"Ping to {host} is {self.ping(host)}")
+        if self.results.full():
+            self.results.get()
+        self.results.put(r)
+    
+    def get_mean_ping(self):
+        """
+        Calculate mean ping time in ms
+        """
+        if self.results.empty():
+            return 999
+        return mean([r["time"] for r in list(self.results.queue)])
+    
+    def get_last_ping(self):
+        """
+        Get last ping time in ms
+        """
+        return list(self.results.queue)[-1]["time"]
+    
+    def init_interface(self):
+        """
+        Init tray icon and interface
+        """
+        self.app.setQuitOnLastWindowClosed(False)
+        self.tray.setIcon(self.icon)
+        self.tray.setVisible(True)
+        menu = QMenu()
+        quit = QAction("Quit")
+        quit.triggered.connect(self.app.quit)
+        menu.addAction(quit)
+        self.tray.setContextMenu(menu)
+
+    def run(self):
+        """
+        Entrypoint
+        """
+        self.app.exec_()
+        # while True:
+        #     started = datetime.now()
+        #     time = self.ping(self.host)
+        #     self.add_result({
+        #         'started': started,
+        #         'time': time
+        #     })
+        #     self.tray.setToolTip(f"Mean ping {self.get_mean_ping()}")
+        #     sleep(self.interval)
 
 if __name__ == '__main__':
     ct = ConnTester()
-    ct.ping_all()
+    ct.run()
