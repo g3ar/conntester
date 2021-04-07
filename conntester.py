74311658a8ad9294ae5bd718c772081879c8cf9f
gear <misty.g3ar@gmail.com>
2021-04-07 20:55:35 +0300

    Config added.

--- conntester.py
+++ conntester.py
@@ -1,18 +1,31 @@
+import configparser
+
 from ping3 import ping as p3p
 
 
-def ping(host):
-    """
-    Pings specified host and returns ping time in ms
-    """
-    return p3p(host, unit='ms')
+class ConnTester():
+    config = configparser.ConfigParser()
+    hosts = []
+    ping_timeout = 1
 
+    def __init__(self):
+        self.config.read('conntester.ini')
+        self.hosts = self.config.get('MAIN', 'hosts').split(', ')
+        self.ping_timeout = int(self.config.get('MAIN', 'timeout'))
+    
+    def ping(self, host):
+        """
+        Pings specified host and returns ping time in ms
+        """
+        return p3p(host, timeout=self.ping_timeout, unit='ms')
+    
+    def ping_all(self):
+        """
+        Pings all hosts from config
+        """
+        for host in self.hosts:
+            print(f"Ping to {host} is {self.ping(host)}")
 
 if __name__ == '__main__':
-    hosts = {
-        'google': 'google.com',
-        'google_dns': '8.8.8.8',
-        'nonexistent': '192.168.2.2'
-    }
-    for name, host in hosts.items():
-        print(f"Ping to {name} is {ping(host)}")
+    ct = ConnTester()
+    ct.ping_all()
